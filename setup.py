d15d23db8c79b3ff6b0884ea2478b8b2cf0bd400
aviana-michaelj <47867074+aviana-michaelj@users.noreply.github.com>
2019-05-23 18:39:07 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Development of mMachine Learning in Nemesis',
+    description='Development of Machine Learning in Nemesis',
     author='Aviana Global Technologies',
     license='',
 )
