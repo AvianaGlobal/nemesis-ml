54f04a51f9e1f9a98251d7b612702eec19caf9b8
RogerWangAviana <rogerw@avianaglobal.com>
2020-03-12 14:48:29 -0700

    entropy

---
+++ app/models/entropysq.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from scipy import stats
+
+def entropysq(df, control, target):
+  classlist = df[control].unique().tolist()
+  out = pd.DataFrame(columns=[control,'Entropy'])
+  out[str(control)] = classlist
+  out.Entropy = list(map(
+    lambda a: stats.entropy(df.loc[df[str(control)] == a, target].tolist()),classlist))
+  return out
+
+# df = pd.read_csv('card transactions.csv')
+# print(entropysq(df,'Transtype','Fraud'))
