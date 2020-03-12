7c8d951925e4a59dd89efb5ec620b87487562b43
Xiaoyan Lin <xiaoyanl@avianaglobal.com>
2020-03-12 14:00:28 -0700

    principal feature analysis

---
+++ app/models/PFA.py
@@ -0,0 +1,47 @@
+# PFA is a feature selection method. It reduces dimension of a feature set by
+# choosing a subset of the original features that contains most of the
+# essential information, using the same criteria as PCA
+
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from collections import defaultdict
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.preprocessing import StandardScaler
+
+class PFA(object):
+    def __init__(self, n_features, q=None):
+        self.q = q
+        self.n_features = n_features
+
+    def fit(self, X):
+        if not self.q:
+            self.q = X.shape[1]
+
+        sc = StandardScaler()
+        X = sc.fit_transform(X)
+
+        pca = PCA(n_components=self.q).fit(X)
+        A_q = pca.components_.T
+
+        kmeans = KMeans(n_clusters=self.n_features).fit(A_q)
+        clusters = kmeans.predict(A_q)
+        cluster_centers = kmeans.cluster_centers_
+
+        dists = defaultdict(list)
+        for i, c in enumerate(clusters):
+            dist = euclidean_distances([A_q[i, :]], [cluster_centers[c, :]])[0][0]
+            dists[c].append((i, dist))
+
+        self.indices_ = [sorted(f, key=lambda x: x[1])[0][0] for f in dists.values()]
+        self.features_ = X[:, self.indices_]
+
+
+
+# pfa = PFA(n_features=10)
+# pfa.fit(X)
+
+# To get the transformed matrix
+# X = pfa.features_
+
+# To get the column indices of the kept features
+# column_indices = pfa.indices_
