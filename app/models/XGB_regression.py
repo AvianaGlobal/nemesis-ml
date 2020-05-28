439b9d0f2517de0f112cf48684eaa1ac5f135df9
Xiaoyan Lin <xiaoyanl@avianaglobal.com>
2020-05-27 18:49:05 -0700

    Sklearn LR

--- app/models/XGB_regression.py
+++ app/models/XGB_regression.py
@@ -1,53 +1,107 @@
 import pickle
-import warnings
+import warning
+warnings.filterwarnings('ignore')
+import numpy as np
 import pandas as pd
+from datetime import datetime
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, GridSearchCV
+from sklearn.metrics import roc_auc_score, r2_score, mean_squared_error
+from sklearn.model_selection import StratifiedKFold
+from xgboost.sklearn import XGBRegressor
 from matplotlib import pyplot as plt
 from xgboost import plot_importance
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_squared_error
-from xgboost.sklearn import XGBRegressor
-
-warnings.filterwarnings("ignore")
 
 
 # import dataset
 def main():
     data_file_name = input('Data file name: ')
-    data_type_file_name = input('Column type file name: ')
-    df = pd.read_csv('../../data/processed/' + data_file_name + '.csv')
-    df_type = pd.read_csv('../../data/processed/' + data_type_file_name + '.csv')
-    file = open('../../reports/build_models/' + data_file_name + '_XGB_regression_report.txt', 'w')
-    XGB_Regression(file, df, df_type, data_file_name)
+    target = input('What is the column name of the target: : ')
+    while True:
+        tune = input('Do you want to tune model parameters? Y/N: ')
+        if tune == 'Y' or tune == 'N':
+            break
+    df = pd.read_csv(data_file_name + '.csv')
+    file = open(data_file_name + '_XGB_regression_report.txt', 'w')
+    XGB_Regression(file, df, target, data_file_name, tune)
     file.close()
 
 
-def XGB_Regression(file, df, df_type, data_file_name):
+def XGB_Regression(file, df, target, data_file_name, tune, test_size=0.2):
+    def timer(start_time=None):
+        if not start_time:
+            start_time = datetime.now()
+            return start_time
+        elif start_time:
+            thour, temp_sec = divmod((datetime.now() - start_time).total_seconds(), 3600)
+            tmin, tsec = divmod(temp_sec, 60)
+            print('\n Time taken: %i hours %i minutes and %s seconds.' % (thour, tmin, round(tsec, 2)))
+
     file.write('The current dataset has' + str(df.shape[1]) + ' columns and ' + str(df.shape[0]) + ' rows')
     file.write('Column names are ' + str(df.columns) + '\n')
 
-    # funtion to get column type
-    def column_type(column_name, df_type):
-        return df_type.loc[df_type['Variable'] == column_name, 'Type'].iloc[0]
-
-    # Function to get target variable
-    def get_target(df, df_type):
-        for c in df:
-            if column_type(c, df_type) == 'Flag_Continuous' or column_type(c, df_type) == 'Flag_Categorical':
-                return c
-
-    target_name = get_target(df, df_type)
-    X = df.drop(columns=target_name)
-    y = df[target_name]
+    X = df.drop(columns=target)
+    y = df[target]
 
     # seperate train set (80%) and test set (20%)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=0)
 
-    # Instantiate and train the model
-    model = XGBRegressor(max_depth=1, learning_rate=0.1, n_estimators=100, silent=True, objective='reg:linear')
+    if tune == 'Y':
+        # A parameter grid for XGBoost
+        params = {
+            'min_child_weight': [1, 5, 10],
+            'gamma': [0.5, 1, 2, 5],
+            'subsample': [0.6, 1.0],
+            'colsample_bytree': [0.6, 1.0],
+            'max_depth': [3, 5],
+            'n_estimators': [100, 300, 500]
+        }
+
+        # Instantiate and train the model
+
+        model = XGBRegressor(max_depth=1, learning_rate=0.1, n_estimators=100, silent=True, objective='reg:linear')
+
+        folds = 3
+        param_comb = 5
+
+        skf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=0)
+
+        random_search = RandomizedSearchCV(model, param_distributions=params, n_iter=param_comb,
+                                           scoring='neg_mean_squared_error', n_jobs=4, cv=skf.split(X_train, y_train),
+                                           verbose=3, random_state=0)
+
+        # Here we go
+        start_time = timer(None)  # timing starts from this point for "start_time" variable
+        random_search.fit(X_train, y_train)
+        timer(start_time)
+
+        print('\n All results:')
+        print(random_search.cv_results_)
+        print('\n Best estimator:')
+        print(random_search.best_estimator_)
+        print('\n Best normalized gini score for %d-fold search with %d parameter combinations:' % (folds, param_comb))
+        print(random_search.best_score_ * 2 - 1)
+        print('\n Best hyperparameters:')
+        print(random_search.best_params_)
+        results = pd.DataFrame(random_search.cv_results_)
+        # save the model to disk
+        results.to_csv('random-grid-search-xgb.csv', index=False)
+
+        model = XGBRegressor(max_depth=random_search.best_params_['max_depth'], \
+                             learning_rate=0.1, \
+                             n_estimators=random_search.best_params_['n_estimators'], \
+                             min_child_weight=random_search.best_params_['min_child_weight'], \
+                             gamma=random_search.best_params_['gamma'], \
+                             subsample=random_search.best_params_['subsample'], \
+                             colsample_bytree=random_search.best_params_['colsample_bytree'], \
+                             silent=True, objective='reg:linear')
+
+    else:
+        model = XGBRegressor(max_depth=1, learning_rate=0.1, n_estimators=100, silent=True, objective='reg:linear')
+
     model.fit(X_train, y_train)
 
     # save the model to disk
-    filename = '../../models/' + data_file_name + '_XGBRegressor.sav'
+    filename = data_file_name + '_XGBRegressor.sav'
     pickle.dump(model, open(filename, 'wb'))
 
     # prediction
--- app/models/XGClassifier_T.py
+++ app/models/XGClassifier_T.py
@@ -1,62 +1,114 @@
 import pickle
-import xgboost as xgb
+import warnings
+
+warnings.filterwarnings('ignore')
+import numpy as np
 import pandas as pd
+import xgboost as xgb
 from matplotlib import pyplot as plt
 from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from sklearn import metrics
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, GridSearchCV
+from sklearn.metrics import roc_auc_score, r2_score, mean_squared_error, accuracy_score
+from sklearn.model_selection import StratifiedKFold
+from matplotlib import pyplot as plt
+from xgboost import plot_importance
 
 
+# import dataset
 def main():
     data_file_name = input('Data file name: ')
-    data_type_file_name = input('Column type file name: ')
-    df = pd.read_csv('../../data/processed/' + data_file_name + '.csv')
-    df_type = pd.read_csv('../../data/processed/' + data_type_file_name + '.csv')
-    file = open('../../reports/build_models/' + data_file_name + '_XGB_Classifier_report.txt', 'w')
-    XGB_Classifier(file, df, df_type, data_file_name)
+    target = input('What is the column name of the target: : ')
+    while True:
+        tune = input('Do you want to tune model parameters? Y/N: ')
+        if tune == 'Y' or tune == 'N':
+            break
+    df = pd.read_csv(data_file_name + '.csv')
+    file = open(data_file_name + '_XGB_Classifier_report.txt', 'w')
+    XGB_Classifier(file, df, target, data_file_name, tune)
     file.close()
 
 
-def XGB_Classifier(file, dataset, dataset_type, data_file_name, test_size=0.2):
-    # Function to get target variable
-    def get_target(df, df_type):
-        for c in df:
-            if column_type(c, df_type) == 'Flag_Continuous' or column_type(c, df_type) == 'Flag_Categorical':
-                return c
+def XGB_Classifier(file, df, target, data_file_name, tune, test_size=0.2):
+    def timer(start_time=None):
+        if not start_time:
+            start_time = datetime.now()
+            return start_time
+        elif start_time:
+            thour, temp_sec = divmod((datetime.now() - start_time).total_seconds(), 3600)
+            tmin, tsec = divmod(temp_sec, 60)
+            print('\n Time taken: %i hours %i minutes and %s seconds.' % (thour, tmin, round(tsec, 2)))
 
-    # funtion to get column type
-    def column_type(column_name, df_type):
-        return df_type.loc[df_type['Variable'] == column_name, 'Type'].iloc[0]
+    file.write('The current dataset has' + str(df.shape[1]) + ' columns and ' + str(df.shape[0]) + ' rows')
+    file.write('Column names are ' + str(df.columns) + '\n')
 
-    file.write('1. The current dataset has' + str(dataset.shape[1]) + ' columns and ' + str(dataset.shape[0]) + ' rows')
-    file.write('\n' + 'Column names are ' + str(list(dataset.columns)) + '\n')
-    file.write('\n' + 'The first 10 rows of this dataset: ' + '\n' + '\n' + str(dataset.head(10)) + '\n' + '\n')
+    X = df.drop(columns=target)
+    y = df[target]
 
-    # split data into X and Y
-    target_name = get_target(dataset, dataset_type)
-    X = dataset.drop(columns=target_name)
-    Y = dataset[target_name]
+    # seperate train set (80%) and test set (20%)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=0)
 
-    # split data into train and test sets
-    seed = 0
-    test_size = test_size
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+    if tune == 'Y':
+        # A parameter grid for XGBoost
+        params = {
+            'min_child_weight': [1, 5, 10],
+            'gamma': [0.5, 1, 2, 5],
+            'subsample': [0.6, 1.0],
+            'colsample_bytree': [0.6, 1.0],
+            'max_depth': [3, 5],
+            'n_estimators': [100, 300, 500]
+        }
 
-    # fit model no training data
-    model = XGBClassifier()
-    model.fit(x_train, y_train)
+        # Instantiate and train the model
+
+        model = XGBClassifier()
+
+        folds = 3
+        param_comb = 5
+
+        skf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=0)
+
+        random_search = RandomizedSearchCV(model, param_distributions=params, n_iter=param_comb, scoring='roc_auc',
+                                           n_jobs=4, cv=skf.split(X_train, y_train), verbose=3, random_state=0)
+
+        # Here we go
+        start_time = timer(None)  # timing starts from this point for "start_time" variable
+        random_search.fit(X_train, y_train)
+        timer(start_time)
+
+        print('\n All results:')
+        print(random_search.cv_results_)
+        print('\n Best estimator:')
+        print(random_search.best_estimator_)
+        print('\n Best normalized gini score for %d-fold search with %d parameter combinations:' % (folds, param_comb))
+        print(random_search.best_score_ * 2 - 1)
+        print('\n Best hyperparameters:')
+        print(random_search.best_params_)
+        results = pd.DataFrame(random_search.cv_results_)
+        # save the model to disk
+        results.to_csv('classifier-random-grid-search-xgb.csv', index=False)
+
+        model = XGBClassifier(max_depth=random_search.best_params_['max_depth'], \
+                              learning_rate=0.1, \
+                              n_estimators=random_search.best_params_['n_estimators'], \
+                              min_child_weight=random_search.best_params_['min_child_weight'], \
+                              gamma=random_search.best_params_['gamma'], \
+                              subsample=random_search.best_params_['subsample'], \
+                              colsample_bytree=random_search.best_params_['colsample_bytree'], \
+                              silent=True, objective='reg:linear')
+
+    else:
+        model = XGBClassifier()
+
+    model.fit(X_train, y_train)
+
     # save the model to disk
-    filename = '../../models/' + data_file_name + '_XGBClassifier.sav'
+    filename = data_file_name + '_XGBRegressor.sav'
     pickle.dump(model, open(filename, 'wb'))
-
-    # important features
-    xgb.plot_importance(model)
-    plt.show()
 
     # prediction
     loaded_model = pickle.load(open(filename, 'rb'))
-    y_pred = loaded_model.predict(x_test)
+    y_pred = loaded_model.predict(X_test)
 
     # print the confusion matrix
     cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
