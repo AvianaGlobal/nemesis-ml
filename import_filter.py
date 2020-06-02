26f460ee083eeaed7e95d0885da66a5cfec759ea
RogerWangAviana <rogerw@avianaglobal.com>
2020-06-02 13:00:50 -0700

    data preparation module

--- import_filter.py
+++
@@ -1,6 +0,0 @@
-# allow users to enter their filter
-# data should be a pandas dataframe and criterion should follow pandas syntax
-
-def filter(data, criterion):
-    data = data[criterion]
-    return data
